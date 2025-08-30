db4d16a947521712d3820659712fb97b1f45d72f
Omar L. <github@olh.xyz>
2025-08-30 18:09:23 +0200

    fzf's key-bindings.bash: add function to launch fzf with ripgrep

--- dot-local/share/fzf/key-bindings.bash
+++ dot-local/share/fzf/key-bindings.bash
@@ -17,13 +17,30 @@ __fzf_select__() {
     fi
 }
 
+__fzf_ripgrep__() {
+    RELOAD='reload:rg --column --color=always --smart-case {q} || :'
+    ACTION='echo {+1}'
+
+    fzf --disabled --ansi --border-label=" Search in $PWD " \
+        --bind "start:$RELOAD" --bind "change:$RELOAD" \
+        --bind "enter:become:$ACTION" \
+        --delimiter :
+}
+
 fzf-file-widget() {
-    local selected="$(__fzf_select__ $1)"
+    if [[ -n $1 ]]; then
+        local selected="$(__fzf_select__ $1)"
+    else
+        local selected="$(__fzf_ripgrep__)"
+    fi
     READLINE_LINE="${READLINE_LINE:0:$READLINE_POINT}$selected${READLINE_LINE:$READLINE_POINT}"
     READLINE_POINT=$(( READLINE_POINT + ${#selected} ))
 }
 
 # readline bindings to paste the selected file path into the command line
+# CTRL-f CTRL-g - Start search from the current dir.
+bind -m vi-command -x '"\C-f\C-g": fzf-file-widget'
+bind -m vi-insert -x '"\C-f\C-g": fzf-file-widget'
 # CTRL-f CTRL-f - Start search from the current dir.
 bind -m vi-command -x '"\C-f\C-f": fzf-file-widget $HOME'
 bind -m vi-insert -x '"\C-f\C-f": fzf-file-widget $HOME'
