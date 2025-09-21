82f118f8865ea9e56091475b2c7a2507ef302b39
Omar L. <github@olh.xyz>
2025-09-21 16:47:38 +0200

    fzf's key-bindings.bash: add function and bindings for fuzzy searching the command history

--- dot-local/share/fzf/key-bindings.bash
+++ dot-local/share/fzf/key-bindings.bash
@@ -32,6 +32,28 @@ __fzf_ripgrep__() {
         --delimiter :
 }
 
+__fzf_history__() {
+    local output script
+
+    script='BEGIN { getc; $/ = "\n\t"; $HISTCOUNT = $ENV{last_hist} + 1 } s/^[ *]//; s/\n/\n\t/gm; print $HISTCOUNT - $. . "\t$_" if !$seen{$_}++'
+    output=$(
+        set +o pipefail
+        builtin fc -lnr -2147483648 |
+        last_hist=$(HISTTIMEFORMAT='' builtin history 1) command perl -n -l0 -e "$script" |
+        fzf --read0 --nth 2.. --scheme history --no-multi --query "$READLINE_LINE" \
+            --wrap --wrap-sign '	↳' --header 'CTRL-S (toggle sorting)' \
+            --header-border bottom --border-label " Search the command history 📋 " \
+            --bind=ctrl-s:toggle-sort --preview ''
+    ) || return
+
+    READLINE_LINE=$(command perl -pe 's/^\d*\t//' <<< "$output")
+    if [[ -z "$READLINE_POINT" ]]; then
+        echo "$READLINE_LINE"
+    else
+        READLINE_POINT=0x7fffffff
+    fi
+}
+
 fzf-file-widget() {
     if [[ -n $1 ]]; then
         local selected="$(__fzf_select__ $1)"
@@ -55,3 +77,6 @@ bind -m vi-insert -x '"\C-f\C-r": fzf-file-widget /'
 # CTRL-/ - Search files starting from the home dir.
 bind -m vi-command -x '"\C-_": fzf-file-widget $PWD'
 bind -m vi-insert -x '"\C-_": fzf-file-widget $PWD'
+# CTRL-R - Search commands in bash's command hisory
+bind -m vi-command -x '"\C-r": __fzf_history__'
+bind -m vi-insert -x '"\C-r": __fzf_history__'
